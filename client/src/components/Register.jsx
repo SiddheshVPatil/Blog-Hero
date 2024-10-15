199e5608b7a41935f4d4b74389c1efd754d8a9d6
Sid <104507303+SiddheshVPatil@users.noreply.github.com>
2024-10-15 23:09:26 +0530

    Update Register.jsx

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -8,7 +8,7 @@ export default function Register() {
   const handleSubmit=async (e)=>{
     e.preventDefault();
 
-    const response =await fetch('http://localhost:5000/user/register',{
+    const response =await fetch('https://blog-hero.onrender.com/user/register',{
       method:'POST',
       headers:{'Content-Type':'application/json'},
       body: JSON.stringify({name,password})
